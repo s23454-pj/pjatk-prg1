d70ccafe1b21cbfead154367ec841ff64e70ff7d
Karol Jagodzinski <s23454@pjwstk.edu.pl>
2020-10-22 16:06:44 +0200

    [02-first-steps] div

---
+++ src/s01-div.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+
+auto main(int argc, char* argv[]) -> int
+{
+    auto const a = std::stof(argv[1]);
+    auto const b = std::stof(argv[2]);
+    std::cout << a/b << std::endl;
+
+    return 0;
+}
