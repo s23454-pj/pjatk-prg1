9e6417b054e6964da999cf9960ec30077919879c
Karol Jagodzinski <s23454@pjwstk.edu.pl>
2020-10-23 21:26:04 +0200

    [02-first-steps] Better catching unused args in main function

--- src/s01-add.cpp
+++ src/s01-add.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 
 
-auto main(int argc, char* argv[]) -> int
+auto main(int, char* argv[]) -> int
 {
-    (void)argc;
     auto const a = std::stoi(argv[1]);
     auto const b = std::stoi(argv[2]);
     std::cout << (a + b) << std::endl;
--- src/s01-div.cpp
+++ src/s01-div.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 
 
-auto main(int argc, char* argv[]) -> int
+auto main(int, char* argv[]) -> int
 {
-    (void)argc;
     auto const a = std::stof(argv[1]);
     auto const b = std::stof(argv[2]);
     std::cout << a / b << std::endl;
