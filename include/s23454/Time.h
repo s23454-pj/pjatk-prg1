69f92f941535ed5e451f48db3a79f6b0d317bf0a
Karol Jagodzinski <s23454@pjwstk.edu.pl>
2020-12-02 11:28:38 +0100

    [3] Fix Time - move implementation outside struct. Fix `time_of_day`

--- include/s23454/Time.h
+++ include/s23454/Time.h
@@ -1,23 +1,15 @@
 #ifndef S23454_TIME_H
 #define S23454_TIME_H
-#include "sstream"
-
-auto prepare_unit_to_print(int unit) -> std::string
-{
-    if (unit < 10) {
-        return "0" + std::to_string(unit);
-    }
-    return std::to_string(unit);
-}
+#include <iostream>
 
 namespace s23454 {
 struct Time {
     int hh, mm, ss;
-    enum class Time_of_day { morning = 1, day = 2, evening = 3, night = 4 };
+    enum class Time_of_day { morning, day, evening, night, not_set };
 
     Time(int, int, int);
-    auto to_string() const -> std::string;
-    auto time_of_day() const -> std::string;
+    auto to_string(enum Time_of_day) const -> std::string;
+    auto time_of_day() const -> Time_of_day;
     auto next_hour();
     auto next_minute();
     auto next_second();
@@ -31,144 +23,6 @@ struct Time {
     auto operator==(Time const&) -> bool;
     auto operator!=(Time const&) -> bool;
 };
-
-auto Time::to_string() const -> std::string
-{
-    auto out = std::ostringstream{};
-    out << prepare_unit_to_print(hh) << ":" << prepare_unit_to_print(mm) << ":"
-        << prepare_unit_to_print(ss) << "\n";
-    return out.str();
-}
-
-auto Time::time_of_day() const -> std::string
-{
-    return "Foo";
-}
-
-
-auto Time::next_hour()
-{
-    ++hh;
-    if (hh > 23) {
-        hh = 0;
-    }
-}
-auto Time::next_minute()
-{
-    ++mm;
-    if (mm > 59) {
-        mm = 0;
-        next_hour();
-    }
-}
-auto Time::next_second()
-{
-    ++ss;
-    if (ss > 59) {
-        ss = 0;
-        next_minute();
-    }
-}
-
-auto Time::count_seconds() const -> uint64_t
-{
-    return ss + mm * 60 + hh * 3600;
-}
-
-auto Time::count_minutes() const -> uint64_t
-{
-    return ss / 60 + mm + hh * 60;
-}
-
-auto Time::time_to_midnight() -> Time
-{
-    int seconds = 0;
-    int minutes = 0;
-    int hours   = 0;
-
-    while (ss != 0) {
-        next_second();
-        ++seconds;
-    }
-    while (mm != 0) {
-        next_minute();
-        ++minutes;
-    }
-    while (hh != 0) {
-        next_hour();
-        ++hours;
-    }
-    hh = hours;
-    mm = minutes;
-    ss = seconds;
-
-
-    return *this;
-}
-
-
-auto Time::operator+(Time const& time) -> Time
-{
-    for (int i = 0; i < time.ss; ++i) {
-        next_second();
-    };
-    for (int i = 0; i < time.mm; ++i) {
-        next_minute();
-    };
-    for (int i = 0; i < time.hh; ++i) {
-        next_hour();
-    };
-    return *this;
-};
-
-auto Time::operator-(Time const& time) -> Time
-{
-    auto diff_ss = ss - time.ss;
-    auto diff_mm = mm - time.mm;
-    auto diff_hh = hh - time.hh;
-
-    if (diff_ss < 0) {
-        --diff_mm;
-        ss = 60 + diff_ss;
-    }
-
-    if (diff_mm < 0) {
-        --diff_hh;
-        mm = 60 + diff_mm;
-    }
-
-    if (diff_hh < 0) {
-        hh = 24 + diff_hh;
-    }
-    return *this;
-}
-
-auto Time::operator<(Time const& time) -> bool
-{
-    if (hh == time.hh and mm == time.mm) {
-        return ss < time.ss;
-    }
-    if (hh == time.hh) {
-        return mm < time.hh;
-    }
-    return ss < time.ss;
-}
-
-auto Time::operator>(Time const& time) -> bool
-{
-    if (hh == time.hh and mm == time.mm) {
-        return ss > time.ss;
-    }
-    if (hh == time.hh) {
-        return mm > time.hh;
-    }
-    return ss > time.ss;
-}
-auto Time::operator==(Time const& time) -> bool
-{
-    return (hh == time.hh and mm == time.mm and ss == time.ss);
-}
-
 
 }  // namespace s23454
 
--- src/s03-Time.cpp
+++ src/s03-Time.cpp
@@ -1,8 +1,169 @@
-#include "iostream"
-#include "s23454/Time.h"
+#include <s23454/Time.h>
 
+#include <iomanip>
+#include <iostream>
+#include <sstream>
 s23454::Time::Time(int hh, int mm, int ss) : hh{hh}, mm{mm}, ss{ss}
 {}
+
+
+auto prepare_unit_to_print(int unit) -> std::string
+{
+    auto out = std::ostringstream{};
+    out << std::setfill('0') << std::setw(2) << unit;
+    return out.str();
+}
+
+auto s23454::Time::to_string(enum s23454::Time::Time_of_day time_of_day =
+                                 s23454::Time::Time_of_day::not_set) const
+    -> std::string
+{
+    auto out = std::ostringstream{};
+    if (time_of_day != Time_of_day::not_set) {
+        switch (time_of_day) {
+        case Time_of_day::morning:
+            out << "Morning";
+            break;
+        case Time_of_day::day:
+            out << "Day";
+            break;
+        case Time_of_day::evening:
+            out << "Evening";
+            break;
+        case Time_of_day::night:
+            out << "Night";
+            break;
+        default:
+            break;
+        }
+        out << "\n";
+    } else {
+        out << prepare_unit_to_print(hh) << ":" << prepare_unit_to_print(mm)
+            << ":" << prepare_unit_to_print(ss) << "\n";
+    }
+    return out.str();
+}
+
+
+auto s23454::Time::time_of_day() const -> Time::Time_of_day
+{
+    if (hh <= 5) {
+        return Time_of_day::night;
+    } else if (hh <= 10) {
+        return Time_of_day::morning;
+    } else if (hh <= 18) {
+        return Time_of_day::day;
+    } else if (hh <= 22) {
+        return Time_of_day::evening;
+    } else {
+        return Time_of_day::night;
+    }
+}
+
+
+auto s23454::Time::next_hour()
+{
+    ++hh;
+    if (hh > 23) {
+        hh = 0;
+    }
+}
+auto s23454::Time::next_minute()
+{
+    ++mm;
+    if (mm > 59) {
+        mm = 0;
+        next_hour();
+    }
+}
+auto s23454::Time::next_second()
+{
+    ++ss;
+    if (ss > 59) {
+        ss = 0;
+        next_minute();
+    }
+}
+
+auto s23454::Time::count_seconds() const -> uint64_t
+{
+    return ss + mm * 60 + hh * 3600;
+}
+
+
+auto s23454::Time::count_minutes() const -> uint64_t
+{
+    return ss / 60 + mm + hh * 60;
+}
+
+auto s23454::Time::time_to_midnight() -> Time
+{
+    return Time{0, 0, 0} - *this;
+}
+
+
+auto s23454::Time::operator+(Time const& time) -> Time
+{
+    for (int i = 0; i < time.ss; ++i) {
+        next_second();
+    };
+    for (int i = 0; i < time.mm; ++i) {
+        next_minute();
+    };
+    for (int i = 0; i < time.hh; ++i) {
+        next_hour();
+    };
+    return *this;
+};
+
+auto s23454::Time::operator-(Time const& time) -> Time
+{
+    auto diff_ss = ss - time.ss;
+    auto diff_mm = mm - time.mm;
+    auto diff_hh = hh - time.hh;
+
+    if (diff_ss < 0) {
+        --diff_mm;
+        ss = 60 + diff_ss;
+    }
+
+    if (diff_mm < 0) {
+        --diff_hh;
+        mm = 60 + diff_mm;
+    }
+
+    if (diff_hh < 0) {
+        hh = 24 + diff_hh;
+    }
+    return *this;
+}
+
+auto s23454::Time::operator<(Time const& time) -> bool
+{
+    if (hh == time.hh and mm == time.mm) {
+        return ss < time.ss;
+    }
+    if (hh == time.hh) {
+        return mm < time.hh;
+    }
+    return ss < time.ss;
+}
+
+auto s23454::Time::operator>(Time const& time) -> bool
+{
+    if (hh == time.hh and mm == time.mm) {
+        return ss > time.ss;
+    }
+    if (hh == time.hh) {
+        return mm > time.hh;
+    }
+    return ss > time.ss;
+}
+auto s23454::Time::operator==(Time const& time) -> bool
+{
+    return (hh == time.hh and mm == time.mm and ss == time.ss);
+}
+
 
 auto main() -> int
 {
@@ -24,6 +185,10 @@ auto main() -> int
     time = s23454::Time{23, 59, 59};
     time.next_second();
     std::cout << time.to_string() << "\n\n";
+
+    std::cout << "time_of_day()\n";
+    time = s23454::Time{23, 59, 59};
+    std::cout << time.to_string(time.time_of_day()) << "\n";
 
     std::cout << "+\n";
     auto a = s23454::Time{23, 59, 59};
@@ -62,7 +227,7 @@ auto main() -> int
     std::cout << time.count_minutes() << "\n\n";
 
     std::cout << "time_to_midnight()\n";
-    time = s23454::Time{22, 50, 10};
+    time = s23454::Time{23, 59, 59};
     std::cout << time.time_to_midnight().to_string() << "\n\n";
 
     return 0;
